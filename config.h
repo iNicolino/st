0114dc30211de2b4f9248cd252c143848cd7bf6a
iNicolino <inicooonicolino@gmail.com>
2021-11-22 23:11:42 -0300

    changed font

--- config.h
+++ config.h
@@ -5,10 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Ubuntu Mono:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"HanaMinB:pixelsize=14:antialias=true:autohint=true"
+	"Dejavu Sans Mono:pixelsize=14:antialias=true:autohint=true",
+	"HanaMinB:pixelsize=14:antialias=true:autohint=true",
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
